138e6e1a94a4c593c518e03fadf6684bbc2e8e4c
Wojciech Smoliński <wojsmol@wp.pl>
2019-04-21 05:13:34 +0200

    rename custom rulset file + phpcbf

--- server-command.php
+++ server-command.php
@@ -9,11 +9,15 @@ if ( file_exists( $wpcli_server_autoloader ) ) {
 	require_once $wpcli_server_autoloader;
 }
 
-WP_CLI::add_command( 'server', 'Server_Command', array(
-	'before_invoke' => function() {
-		$min_version = '5.4';
-		if ( version_compare( PHP_VERSION, $min_version, '<' ) ) {
-			WP_CLI::error( "The `wp server` command requires PHP {$min_version} or newer." );
-		}
-	}
-) );
+WP_CLI::add_command(
+	'server',
+	'Server_Command',
+	array(
+		'before_invoke' => function() {
+			$min_version = '5.4';
+			if ( version_compare( PHP_VERSION, $min_version, '<' ) ) {
+				WP_CLI::error( "The `wp server` command requires PHP {$min_version} or newer." );
+			}
+		},
+	)
+);
